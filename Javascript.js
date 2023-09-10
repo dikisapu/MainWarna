cbc0772bbc00ed1af06451a737daa0a465dea879
dikisapu <dikis7256@gmail.com>
2023-09-10 20:23:52 +0700

    first Commit

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+const tombol = document.querySelector("#warna");
+
+tombol.addEventListener("click", () => {
+  document.body.classList.toggle("basing");
+});
+// tombol.addEventListener("mouseleave", () => {
+//   document.body.style.backgroundColor = "white";
+// });
+// tombol.classList.toggle("warna");
